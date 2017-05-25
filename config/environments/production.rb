4065ea2b550aacbed4e8e1a4de154e1fab623ea3
Nicholas Major <nick@nmajor.com>
2017-05-25 10:18:45 +0100

    Fixing env prod bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY']
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: "https://lw-mailer.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
